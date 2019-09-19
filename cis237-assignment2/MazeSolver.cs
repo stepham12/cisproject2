9648d4883c939862f2f68b7995b5200835058cf2
David Barnes <dbarnes@kvcc.edu>
2019-09-19 16:13:15 -0400

    Initial commit

---
+++ cis237-assignment2/MazeSolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace cis237_assignment2
+{
+    /// <summary>
+    /// This class is used for solving a char array maze.
+    /// You might want to add other methods to help you out.
+    /// A print maze method would be very useful, and probably neccessary to print the solution.
+    /// If you are real ambitious, you could make a seperate class to handle that.
+    /// </summary>
+    class MazeSolver
+    {
+        /// <summary>
+        /// This is the public method that will allow someone to use this class to solve the maze.
+        /// Feel free to change the return type, or add more parameters if you like, but it can be done
+        /// exactly as it is here without adding anything other than code in the body.
+        /// </summary>
+        public void SolveMaze(char[,] maze, int xStart, int yStart)
+        {
+            // Do work needed to use mazeTraversal recursive call and solve the maze.
+        }
+
+
+        /// <summary>
+        /// This should be the recursive method that gets called to solve the maze.
+        /// Feel free to change the return type if you like, or pass in parameters that you might need.
+        /// This is only a very small starting point.
+        /// More than likely you will need to pass in at a minimum the current position
+        /// in X and Y maze coordinates. EX: mazeTraversal(int currentX, int currentY)
+        /// </summary>
+        private void mazeTraversal()
+        {
+            // Implement maze traversal recursive call
+        }
+    }
+}
---
+++ cis237-assignment2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace cis237_assignment2
+{
+    class Program
+    {
+        /// <summary>
+        /// This is the main entry point for the program.
+        /// You are free to add anything else you would like to this program,
+        /// however the maze solving part needs to occur in the MazeSolver class.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Starting Coordinates.
+            const int X_START = 1;
+            const int Y_START = 1;
+
+            // The first maze that needs to be solved.
+            // Note: You may want to make a smaller version to test and debug with.
+            // You don't have to, but it might make your life easier.
+            char[,] maze1 =
+            { { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
+            { '#', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '#' },
+            { '#', '.', '#', '.', '#', '.', '#', '#', '#', '#', '.', '#' },
+            { '#', '#', '#', '.', '#', '.', '.', '.', '.', '#', '.', '#' },
+            { '#', '.', '.', '.', '.', '#', '#', '#', '.', '#', '.', '.' },
+            { '#', '#', '#', '#', '.', '#', '.', '#', '.', '#', '.', '#' },
+            { '#', '.', '.', '#', '.', '#', '.', '#', '.', '#', '.', '#' },
+            { '#', '#', '.', '#', '.', '#', '.', '#', '.', '#', '.', '#' },
+            { '#', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '#' },
+            { '#', '#', '#', '#', '#', '#', '.', '#', '#', '#', '.', '#' },
+            { '#', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.', '#' },
+            { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' } };
+
+            // Create a new instance of a mazeSolver.
+            MazeSolver mazeSolver = new MazeSolver();
+
+            // Create the second maze by transposing the first maze
+            char[,] maze2 = transposeMaze(maze1);
+
+            // Solve the original maze.
+            mazeSolver.SolveMaze(maze1, X_START, Y_START);
+
+            // Solve the transposed maze.
+            mazeSolver.SolveMaze(maze2, X_START, Y_START);
+
+        }
+
+        /// <summary>
+        /// This method will take in a 2 dimensional char array and return
+        /// a char array maze that is flipped along the diagonal, or in mathematical
+        /// terms, transposed.
+        /// ie. if the array looks like 1, 2, 3
+        ///                             4, 5, 6
+        ///                             7, 8, 9
+        /// The returned result will be:
+        ///                             1, 4, 7
+        ///                             2, 5, 8
+        ///                             3, 6, 9
+        /// The current return statement is just a placeholder so the program
+        /// doesn't complain about no return value.
+        /// 
+        /// It is important that you return a "new" char array as the transposed maze.
+        /// If you do not, you could end up only solving the transposed maze.
+        /// </summary>
+        /// <param name="mazeToTranspose"></param>
+        /// <returns>transposedMaze</returns>
+        static char[,] transposeMaze(char[,] mazeToTranspose)
+        {
+            //Write code her to create a transposed maze.
+            return new char[1, 1];
+        }
+    }
+}
