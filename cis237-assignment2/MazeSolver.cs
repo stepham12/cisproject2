6005f5c8e4347af2b467dc9db56d1d7a442d7dc3
Stephanie Amo <amost42@gmail.com>
2019-09-24 22:43:22 -0400

    Add PrintMaze method to MazeSolver class. Update program.

--- cis237-assignment2/MazeSolver.cs
+++ cis237-assignment2/MazeSolver.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
+//Stephanie Amo
+//CIS 237
+//Due: 10/01/2019
+
 namespace cis237_assignment2
 {
     /// <summary>
     /// This class is used for solving a char array maze.
     /// You might want to add other methods to help you out.
     /// A print maze method would be very useful, and probably neccessary to print the solution.
-    /// If you are real ambitious, you could make a seperate class to handle that.
+    /// If you are real ambitious, you could make a separate class to handle that.
     /// </summary>
     class MazeSolver
     {
@@ -18,6 +22,24 @@ namespace cis237_assignment2
         public void SolveMaze(char[,] maze, int xStart, int yStart)
         {
             // Do work needed to use mazeTraversal recursive call and solve the maze.
+            int currentX = xStart;
+            int currentY = yStart;
+
+            //loop as long as exit is not found, which is when coordinates will be 
+            //outside of the array 
+            while(currentX < maze.GetLength(0) && currentY < maze.GetLength(1))
+            {
+                //Mark the current spot as X
+                maze[currentX, currentY] = 'X';
+
+                //Call mazeTraversal method to obtain next coordinates
+                mazeTraversal(ref currentX,ref currentY);
+
+                //Output the maze configuaration after each move
+                PrintMaze(maze);
+
+            }
+
         }
 
 
@@ -28,9 +50,28 @@ namespace cis237_assignment2
         /// More than likely you will need to pass in at a minimum the current position
         /// in X and Y maze coordinates. EX: mazeTraversal(int currentX, int currentY)
         /// </summary>
-        private void mazeTraversal()
+        private void mazeTraversal(ref int currentX, ref int currentY)
         {
             // Implement maze traversal recursive call
+            currentX++;
+            currentY++;
+        }
+
+        private void PrintMaze(char[,] maze)
+        {
+            string printString = "";
+
+            for (int x = 0;  x < maze.GetLength(0); x++)
+            {
+                for (int y = 0; y < maze.GetLength(0); y++)
+                {
+                    printString += (maze[x,y] + " ");
+                    //execute this code block as long as condition is satisfied 
+                }
+                printString += Environment.NewLine;
+            }
+
+            Console.WriteLine(printString);
         }
     }
 }
